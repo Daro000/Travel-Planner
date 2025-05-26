292047d29ac26ae78d4234ef2cb85ea89726aa16
Daro <twojemail@example.com>
2025-05-26 19:54:34 +0200

    Adding StackPanel and functionality for saving in file

---
+++ TravelPlaner/SummaryWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TravelPlaner;
+
+public partial class SummaryWindow : Window
+{
+    public SummaryWindow()
+    {
+        InitializeComponent();
+    }
+}
